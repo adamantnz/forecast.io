e57f599fc1dba834cbf2fb83abd9c244c25833c3
Adam Gibson <adamericgibson@gmail.com>
2016-01-16 16:30:08 +0000

    tidy up spec, add regex

--- spec/checks_weather_spec.rb
+++ spec/checks_weather_spec.rb
@@ -2,14 +2,9 @@ require "rails_helper"
 feature "user visits homepage" do
   scenario "successfully" do
     visit root_path
-    expect(page).to have_css 'h1', text: 'Weather'
-  end
-
-  feature do "sees some weather data"
-    scenario do "successfully"
-      visit root_path
-      # NOTE: to do
-      expect(page).to have_css 'p', text: 'Location: London'
-    end
+    expect(page).to have_css('p', text: /Summary: \w/)
+    expect(page).to have_css('p', text: /Temperature: \d/)
+    expect(page).to have_css('p', text: /Wind Speed: \d/)
+    expect(page).to have_css('p', text: /Timezone: \w/)
   end
 end

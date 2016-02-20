bcb9ea2672c48fc9cf9ea0262739daa04cb94264
Adam Gibson <adamericgibson@gmail.com>
2016-02-20 14:46:12 +0000

    Update capybara spec

--- spec/checks_weather_spec.rb
+++ spec/checks_weather_spec.rb
@@ -2,6 +2,10 @@ require "rails_helper"
 feature "user visits homepage" do
   scenario "successfully" do
     visit root_path
+    fill_in "Latitude", with: "51.5072"
+    fill_in "Longitude", with: "-0.1275"
+    click_button "Search"
+    expect(page).to have_css('h1', text: "Current Weather for 51.5072, -0.1275")
     expect(page).to have_css('p', text: /Summary: \w/)
     expect(page).to have_css('p', text: /Temperature: \d/)
     expect(page).to have_css('p', text: /Wind Speed: \d/)
